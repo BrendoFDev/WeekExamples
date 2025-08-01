b5d306dea3b9abc710f1c792e45b28a484c1b34e
BrendoFDev <brendo.dkry@gmail.com>
2025-08-01 15:43:31 -0300

    feat: First commit

---
+++ Entities/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstWeekExemples.Entities
+{
+    public abstract class Pizza
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public List<string> Ingredients { get; set; } = new List<string>() {"Massa sovada eu acho", "Extrato de tomate", "Queijo", "Oregano"};
+
+        public virtual void ShowIngredients(List<string> ingredients)
+        {
+            foreach(string ingredient in ingredients)
+            {
+                Console.WriteLine($"- {ingredient}");
+            }
+        }
+
+        public abstract void GetDetails();
+    }
+}
---
+++ Entities/PizzaPortuguesa.cs
@@ -0,0 +1,19 @@
+﻿
+namespace FirstWeekExemples.Entities
+{
+    public class PizzaPortuguesa : Pizza
+    {
+        public override void ShowIngredients(List<string> ingredients)
+        {
+            // base serve para chamar o método ShowIngredients da classe Pai
+            base.ShowIngredients(ingredients);
+
+            // depois você consegue criar o comportamento específico da classe filha
+
+        }
+        public override void GetDetails()
+        {
+            Console.WriteLine("Pizza portuguesa");
+        }
+    }
+}
